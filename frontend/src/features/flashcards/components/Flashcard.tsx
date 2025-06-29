6bcab9536a23e1c007fb468b016650e3fbeab0c4
Moeed Rafi <moeedrafi95@gmail.com>
2025-06-29 14:06:39 +0500

    fix: Flipping UI

--- frontend/src/features/flashcards/components/Flashcard.tsx
+++ frontend/src/features/flashcards/components/Flashcard.tsx
@@ -11,31 +11,25 @@ export const Flashcard = ({ flashcard }: FlashcardProps) => {
   return (
     <div className="h-64" onClick={() => setIsFlipped((prev) => !prev)}>
       <div
-        className={`relative h-full transition-transform duration-500 transform-3d ${
+        className={`relative w-full h-full transition-transform duration-500 tranforms transform-3d ${
           isFlipped ? "rotate-y-180" : ""
         }`}
       >
-        <div className="absolute w-full h-full backface-hidden bg-custom-white rounded-lg shadow-md p-6 flex flex-col rotate-y-180">
-          <p className="text-gray-500 text-sm mb-2">Answer</p>
-          <div className="flex-1 flex items-center justify-center">
-            <h3 className="text-custom-black text-lg font-medium">
-              {flashcard.answer}
-            </h3>
-          </div>
-
+        <div className="absolute w-full h-full backface-hidden bg-custom-white rounded-lg shadow-md p-6 flex flex-col justify-center items-center">
+          <p className="text-gray-500 text-sm mb-2">Question</p>
+          <h3 className="text-custom-black text-lg font-medium text-center">
+            {flashcard.question}
+          </h3>
           <p className="mt-4 text-center text-xs text-gray-400">
             Click to flip
           </p>
         </div>
 
-        <div className="absolute w-full h-full backface-hidden bg-custom-white rounded-lg shadow-md p-6 flex flex-col">
-          <p className="text-gray-500 text-sm mb-2">Question</p>
-          <div className="flex-1 flex items-center justify-center">
-            <h3 className="text-custom-black text-lg font-medium">
-              {flashcard.question}
-            </h3>
-          </div>
-
+        <div className="absolute w-full h-full backface-hidden bg-custom-white rounded-lg shadow-md p-6 flex flex-col rotate-y-180 justify-center items-center">
+          <p className="text-gray-500 text-sm mb-2">Answer</p>
+          <h3 className="text-custom-black text-lg font-medium text-center">
+            {flashcard.answer}
+          </h3>
           <p className="mt-4 text-center text-xs text-gray-400">
             Click to flip
           </p>
