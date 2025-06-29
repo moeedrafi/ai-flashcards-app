a4a3f99b36b7e2bfd12bcf1916832c0fa70429ee
Moeed Rafi <moeedrafi95@gmail.com>
2025-06-29 13:31:27 +0500

    feat: Production Deployment

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = import.meta.env.DEV ? "/api" : import.meta.env.VITE_API_URL;
 
 export const apiClient = async <TRequest = unknown, TResponse = unknown>(
   endpoint: string,
